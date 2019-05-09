c8278a76f821093325434025549b106456d7ac3f
alen1433 <alen2herceg@gmail.com>
2019-05-09 18:01:36 +0200

    a little fix

--- Scripts/teaser.js
+++ Scripts/teaser.js
@@ -129,47 +129,47 @@ setTimeout(function(){
 
 
 // --------------------------------------------------------form function-------------------------------------------------
-// $(function() {
-//   // Get the form.
-//   var form = $('#obrazec');
-//   // Get the messages div.
-//   var formMessages = $('#successMessage');
-//   // Set up an event listener for the contact form.
-//   $(form).submit(function(event) {
-//     // Stop the browser from submitting the form.
-//     event.preventDefault();
-//     // Serialize the form data.
-//     var formData = $(form).serialize();
-//     // Submit the form using AJAX.
-//     $.ajax({
-//       type: 'POST',
-//       url: $(form).attr('action'),
-//       data: formData
-//     }).done(function(response) {
-//         // Make sure that the formMessages div has the 'success' class.
-//         $(successMessage).removeClass('error');
-//         $(successMessage).addClass('success');
-//         // Set the message text.
-//         $(successMessage).text(response);
-//         // Clear the form.
-//         $('#formName').val('');
-//         $('#formEmail').val('');
-//         $('#formPhone').val('');
-//         $('#formCompany').val('');
-//         $('#formMessage').val('');
-//       }).fail(function(data) {
-//         // Make sure that the successMessage div has the 'error' class.
-//         $(successMessage).removeClass('success');
-//         $(successMessage).addClass('error');
-//         // Set the message text.
-//         if (data.responseText !== '') {
-//             $(successMessage).text(data.responseText);
-//         } else {
-//             $(successMessage).text('Oops! An error occured and your message could not be sent.');
-//         }
-//     });
-//   });
-// });
+$(function() {
+  // Get the form.
+  var form = $('#obrazec');
+  // Get the messages div.
+  var formMessages = $('#successMessage');
+  // Set up an event listener for the contact form.
+  $(form).submit(function(event) {
+    // Stop the browser from submitting the form.
+    event.preventDefault();
+    // Serialize the form data.
+    var formData = $(form).serialize();
+    // Submit the form using AJAX.
+    $.ajax({
+      type: 'POST',
+      url: $(form).attr('action'),
+      data: formData
+    }).done(function(response) {
+        // Make sure that the formMessages div has the 'success' class.
+        $(successMessage).removeClass('error');
+        $(successMessage).addClass('success');
+        // Set the message text.
+        $(successMessage).text(response);
+        // Clear the form.
+        $('#formName').val('');
+        $('#formEmail').val('');
+        $('#formPhone').val('');
+        $('#formCompany').val('');
+        $('#formMessage').val('');
+      }).fail(function(data) {
+        // Make sure that the successMessage div has the 'error' class.
+        $(successMessage).removeClass('success');
+        $(successMessage).addClass('error');
+        // Set the message text.
+        if (data.responseText !== '') {
+            $(successMessage).text(data.responseText);
+        } else {
+            $(successMessage).text('Oops! An error occured and your message could not be sent.');
+        }
+    });
+  });
+});
 
 // window.onload = setTimeout(function(){
 //     vid.play();
