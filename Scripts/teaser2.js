fab2baaf3208c2db464e9f748db75ed3192f1b43
alen1433 <alen2herceg@gmail.com>
2019-05-14 22:31:49 +0200

    button age check

--- Scripts/teaser2.js
+++ Scripts/teaser2.js
@@ -1,13 +1,17 @@
 
 // $("#siteform").html('<object data="index12.html"/>');
 
-$(document).keypress(function(event){
-  var keycode = (event.keyCode ? event.keyCode : event.which);
-  if(keycode == '13'){
-      enterSite();
-  }
-});
+// $(document).keypress(function(event){
+//   var keycode = (event.keyCode ? event.keyCode : event.which);
+//   if(keycode == '13'){
+//       enterSite();
+//   }
+// });
+
+function errorSite(){
+    $(".error").html('Niste dovol stari za vstop');
+}
 
 function enterSite(){
-    $("#siteform").html('<object data="index12.html"/>');
+  $("#siteform").html('<object data="index12.html"/>');
 }
