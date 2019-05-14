afce277f3ab9d171a0117c7640262bea78e71f07
alen1433 <alen2herceg@gmail.com>
2019-05-14 20:28:49 +0200

    added age check- missing cookies

--- Scripts/teaser2.js
+++ Scripts/teaser2.js
@@ -1,2 +1,20 @@
 
-$("#siteform").html('<object data="index12.html"/>');
+// $("#siteform").html('<object data="index12.html"/>');
+
+$(document).keypress(function(event){
+  var keycode = (event.keyCode ? event.keyCode : event.which);
+  if(keycode == '13'){
+      enterSite();
+  }
+});
+
+function enterSite(){
+  var input = $("#myAge").val();
+  if(input === ""){
+    $(".error").html("Prosim vnesite starost");
+  }else if(input >= 18){
+    $("#siteform").html('<object data="index12.html"/>');
+  }else{
+    $(".error").html("Niste dovolj stari");
+  }
+}
